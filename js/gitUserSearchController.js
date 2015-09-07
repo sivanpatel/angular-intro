20a3fbb443dbd0a917dbe61ae27765d594300229
Emily Carson <emilyjanecarson@gmail.com>
2015-09-07 18:04:40 +0100

    stubbed search tests

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,22 +1,15 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+
+  var access_token = '1239cb7e2d8af52f750c7ebb8146837182ddf40a'
+
+  var searchResource = $resource('https://api.github.com/search/users');
 
   var self = this;
 
   self.doSearch = function() {
-    self.searchResult = {
-    items: [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      }, 
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ]
-  };
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm }
+    );
   };
 
 }]);
