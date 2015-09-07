57545d847421eb251d9d97869dbb8414957ee7f4
Emily Carson <emilyjanecarson@gmail.com>
2015-09-07 18:06:37 +0100

    altered access token

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,7 +1,5 @@
 githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
 
-  var access_token = '1239cb7e2d8af52f750c7ebb8146837182ddf40a'
-
   var searchResource = $resource('https://api.github.com/search/users');
 
   var self = this;
